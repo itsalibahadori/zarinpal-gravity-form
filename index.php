0b62adfc3521558b9b0555eaebffb53e255757fb
Ali Bahadori <ali.bahadori41@yahoo.com>
2024-01-07 09:41:16 +0330

    Init plugin

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه زرین پال گرویتی فرم
+Plugin URI: https://github.com/AliBahadori41/zarinpal-gravity-form
+Description: درگاه پرداخت زرین پال برای گرویتی فرم   
+Version: 2.8.0
+Author: Ali Bahadori
+Author URI: https://bahadori.dev/
+*/
+if (!defined('ABSPATH')) exit;
+require_once('zarinpal.php');
